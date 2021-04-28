19c5a98d9bbaddac549b962ff09d7450f6033752
Evaldo Klock <neto.nemesis@gmail.com>
2021-04-28 17:02:22 -0400

    Update ApiApplication.java

--- api/src/main/java/com/example/api/ApiApplication.java
+++ api/src/main/java/com/example/api/ApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 public class ApiApplication {
