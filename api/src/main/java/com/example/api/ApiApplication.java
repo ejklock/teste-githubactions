c8d380ccb8dcca03b1545393ade959cb35a7de2b
Evaldo Klock <neto.nemesis@gmail.com>
2021-04-28 16:58:40 -0400

    Update ApiApplication.java

--- api/src/main/java/com/example/api/ApiApplication.java
+++ api/src/main/java/com/example/api/ApiApplication.java
@@ -2,12 +2,27 @@ package com.example.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class ApiApplication {
 
+	 @Value("${spring.profiles.active}")
+  	 private String activeProfile;
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
+	
+	@EventListener(ApplicationReadyEvent.class)
+  	public void stopAplicationAfterSuccessfulStartup() {
+    		log.info("Profile:::: " + activeProfile);
+    		if (activeProfile.equals("ci")) {
+      			log.info("Aplicação em modo CI/CD");
+      			log.info("Aplicação iniciada com sucesso");
+      			log.info("Encerrando...");
+     			 System.exit(0);
+   	 }
+  }
 
 }
