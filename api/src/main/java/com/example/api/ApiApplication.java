1e7acfafcdb22335750b31be10168a70a2fa5ced
Evaldo Klock <neto.nemesis@gmail.com>
2021-06-01 09:09:55 -0400

    feat: release pipeline

--- api/src/main/java/com/example/api/ApiApplication.java
+++ api/src/main/java/com/example/api/ApiApplication.java
@@ -11,24 +11,24 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 @SpringBootApplication
 public class ApiApplication {
 
-	@Value("${spring.profiles.active}")
-  	private String activeProfile;
-	
+	@Value("${spring.profiles.active:local}")
+	private String activeProfile;
+
 	private static final Logger log = LoggerFactory.getLogger(ApiApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-	
+
 	@EventListener(ApplicationReadyEvent.class)
-  	public void stopAplicationAfterSuccessfulStartup() {
-    		log.info("Profile:::: " + activeProfile);
-    		if (activeProfile.equals("ci")) {
-      			log.info("Aplicação em modo CI/CD");
-      			log.info("Aplicação iniciada com sucesso");
-      			log.info("Encerrando...");
-     			 System.exit(0);
-   	 }
-  }
+	public void stopAplicationAfterSuccessfulStartup() {
+		log.info("Profile:::: " + activeProfile);
+		if (activeProfile.equals("ci")) {
+			log.info("Aplicação em modo CI/CD");
+			log.info("Aplicação iniciada com sucesso");
+			log.info("Encerrando...");
+			System.exit(0);
+		}
+	}
 
 }
